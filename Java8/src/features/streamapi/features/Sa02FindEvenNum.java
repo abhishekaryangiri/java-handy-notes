fc07ac3bcdc3eac0aec8db97aef07c648ab7d639
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-11-22 01:13:24 +0530

    even num

--- Java8/src/features/streamapi/features/Sa02FindEvenNum.java
+++ Java8/src/features/streamapi/features/Sa02FindEvenNum.java
@@ -7,7 +7,8 @@ public class Sa02FindEvenNum {
 	public static void main(String[] args) {
 		List<Integer> numList = Arrays.asList(1,2,3,4,5,6,7,22,34,56,100,129);
 		
-		//stream api operator -
+		//stream api : find even num
+		
 		numList.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
 	}
 
