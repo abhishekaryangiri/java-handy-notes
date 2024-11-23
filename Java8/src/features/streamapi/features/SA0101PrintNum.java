fc817ac6345719af0b011862f9a6b34d60e8faba
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-11-23 23:30:21 +0530

    print num

--- Java8/src/features/streamapi/features/SA0101PrintNum.java
+++ Java8/src/features/streamapi/features/SA0101PrintNum.java
@@ -1,5 +1,14 @@
 package features.streamapi.features;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SA0101PrintNum {
+	public static void main(String[] args) {
+		
+		 List<Integer> list = Arrays.asList(1,22,43,55,67,44);
+		 list.stream().forEach(System.out::println);
+		 
+	}
 
 }
