6cc700b3241b3008a2e1cb186b7a53e0af2a3b7e
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-11-28 23:53:43 +0530

    ll

--- JavaBasicProblems/src/basicproblems/Array0201CombineArrayandShort.java
+++ JavaBasicProblems/src/basicproblems/Array0201CombineArrayandShort.java
@@ -1,5 +1,26 @@
 package basicproblems;
 
 public class Array0201CombineArrayandShort {
-
+public static void main(String[] args) {
+	int[] nums1 = {1,2,3,0,0,0,0};
+	int[] nums2 = {2,5,6,7};
+	
+	 List<Integer> combList = new ArrayList<>();
+	 
+	 for( int num : nums1) {
+		 if(num !=0) {
+			 combList.add(num);
+		 }
+	 }
+	 for(int num : nums2) {
+		 if(num!=0) {
+			 combList.add(num);
+		 }
+	 }
+	 
+	 int[] op = combList.stream().mapToInt(Integer::intValue).toArray();
+	 
+	 Arrays.sort(op);
+	
+}
 }
