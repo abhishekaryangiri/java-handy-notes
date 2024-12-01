8a7269dfe244e41869b42f9ccf2b329a66877dd0
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-01 23:44:23 +0530

    dub

--- JavaBasicProblems/src/basicproblems/Array0201CombineArrayandShort.java
+++ JavaBasicProblems/src/basicproblems/Array0201CombineArrayandShort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Array0201CombineArrayandShort {
-	//print
+	//merge array and remove 0 . 
 public static void main(String[] args) {
 	int[] nums1 = {1,2,3,0,0,0,0};
 	int[] nums2 = {2,5,6,7};
--- JavaBasicProblems/src/basicproblems/Array0202FindDublicateNumJava8.java
+++ JavaBasicProblems/src/basicproblems/Array0202FindDublicateNumJava8.java
@@ -1,8 +1,17 @@
+
 package basicproblems;
 
-public class Array0202FindDublicateNumJava8 {
-	public static void main(String[] args) {
-		
-	}
+import java.util.HashSet;
 
+public class Array0202FindDublicateNumJava8 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 50, 40, 20};
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i : nums) {
+            if (!hs.add(i)) {
+                System.out.println("Duplicate No: " + i);
+                break;
+            }
+        }
+    }
 }
