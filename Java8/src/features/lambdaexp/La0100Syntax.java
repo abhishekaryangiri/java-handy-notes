d80b6e546c9d1145e7fbba898f6b19ef8e8203dd
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-03 23:58:58 +0530

    syntax of lambda exp

--- Java8/src/features/lambdaexp/La0100Syntax.java
+++ Java8/src/features/lambdaexp/La0100Syntax.java
@@ -1,5 +1,19 @@
 package features.lambdaexp;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class La0100Syntax {
+	public static void main(String[] args) {
+		List<Integer> nums = Arrays.asList(2,4,5,6,7,8,9,1,23,4);
+		nums.forEach(n -> System.out.print(n + " "));
+	}
 
 }
+
+//LE: is short and simple way to write a small block of code that can be passed as an argument to a method or stored as a variable. Its like an anonymous function(means its dont need a name)
+
+//ex. imagine we have to do something simple, like add two nums or print a msg, nad we dnt want to write a full method for it. LE write it in one quick line.
+//(parameter) -> { body} 
+//() -> System.out.prinbtln("Hello World)
+//(a,b) -> a+b
