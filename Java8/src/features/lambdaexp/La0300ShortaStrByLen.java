1ad803230794d4fdf90384cc89955f222f88d9c9
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-05 23:25:13 +0530

    shorting a str

--- Java8/src/features/lambdaexp/La0300ShortaStrByLen.java
+++ Java8/src/features/lambdaexp/La0300ShortaStrByLen.java
@@ -1,5 +1,12 @@
 package features.lambdaexp;
 
-public class La0300ShortaStrByLen {
+import java.util.Arrays;
+import java.util.List;
 
+public class La0300ShortaStrByLen {
+public static void main(String[] args) {
+	List<String> str = Arrays.asList("Abhi", "Ram", "Suman", "Prateek", "ramaswami");
+	str.sort((s1,s2) -> Integer.compare(s1.length(), s2.length()));
+	System.out.println(str);
+}
 }
