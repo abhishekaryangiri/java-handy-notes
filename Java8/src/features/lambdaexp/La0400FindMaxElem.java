1abe3339e7f196fb6915a62fc3915f39fda6f8fb
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-07 23:59:27 +0530

    max elem

--- Java8/src/features/lambdaexp/La0400FindMaxElem.java
+++ Java8/src/features/lambdaexp/La0400FindMaxElem.java
@@ -1,5 +1,12 @@
 package features.lambdaexp;
 
-public class La0400FindMaxElem {
+import java.util.Arrays;
+import java.util.List;
 
+public class La0400FindMaxElem {
+public static void main(String[] args) {
+	List<Integer> nums = Arrays.asList(20,29,60,120,50,30,80);
+	int max = nums.stream().max((a,b) -> a-b).orElseThrow();
+	System.out.println(max);
+}
 }
