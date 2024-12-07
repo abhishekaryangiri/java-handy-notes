18d68c26cdbdc19f0aede83df31a957e13ca934c
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-07 23:55:42 +0530

    find max

--- Java8/src/features/lambdaexp/La0300ShortaStrByLen.java
+++ Java8/src/features/lambdaexp/La0300ShortaStrByLen.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class La0300ShortaStrByLen {
 public static void main(String[] args) {
 	List<String> str = Arrays.asList("Abhi", "Ram", "Suman", "Prateek", "ramaswami");
-	str.sort((s1,s2) -> Integer.compare(s1.length(), s2.length()));
+	str.sort((a,b) -> Integer.compare(a.length(), b.length()));
 	System.out.println(str);
 }
 }
