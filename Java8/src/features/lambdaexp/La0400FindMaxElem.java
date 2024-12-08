1095f059eb4e7c10205ca7db692b79f2d17c84c7
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-08 23:28:17 +0530

    finf first char

--- Java8/src/features/lambdaexp/La0400FindMaxElem.java
+++ Java8/src/features/lambdaexp/La0400FindMaxElem.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class La0400FindMaxElem {
 public static void main(String[] args) {
 	List<Integer> nums = Arrays.asList(20,29,60,120,50,30,80);
-	int max = nums.stream().max((a,b) -> a-b).orElseThrow();
+	int max = nums.stream().max((a,b) -> a-b).orElseThrow(null);
 	System.out.println(max);
 }
 }
