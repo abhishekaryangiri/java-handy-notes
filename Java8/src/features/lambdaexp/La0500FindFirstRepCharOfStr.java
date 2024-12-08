17368e4ffc549cb7e1ecece0fdae559a01a521cc
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-08 23:36:17 +0530

    find first non rep char

--- Java8/src/features/lambdaexp/La0500FindFirstRepCharOfStr.java
+++ Java8/src/features/lambdaexp/La0500FindFirstRepCharOfStr.java
@@ -1,5 +1,24 @@
 package features.lambdaexp;
 
-public class La0500FindFirstRepCharOfStr {
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+public class La0500FindFirstRepCharOfStr {
+public static void main(String[] args) {
+	String str = "india";
+	Character result = str.chars()
+			.mapToObj(c->(char) c)
+			.collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new, Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(entry -> entry.getValue() == 1)
+			.map(Map.Entry::getKey)
+			.findFirst()
+			.orElse(null);
+	
+	System.out.println(result);
+}
 }
