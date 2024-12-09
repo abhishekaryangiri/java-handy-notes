2bdc0add51f235f81ad5e19c79924a23868611e9
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-09 23:52:56 +0530

    sorting

--- Java8/src/features/lambdaexp/La0101SortingaArr.java
+++ Java8/src/features/lambdaexp/La0101SortingaArr.java
@@ -1,5 +1,11 @@
 package features.lambdaexp;
 
-public class La0101SortingaArr {
+import java.util.Arrays;
 
+public class La0101SortingaArr {
+public static void main(String[] args) {
+	int[] arr = {2,7,5,9,90,20,4,0};
+	Arrays.sort(arr);
+	Arrays.stream(arr).forEach(x -> System.out.print(x + " "));
+}
 }
