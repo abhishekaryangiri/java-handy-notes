a985010df5ee36e8b0147253c68d43885cfdaa1e
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-11 23:31:10 +0530

    calculation and print

--- JavaBasicProblems/src/loop/PrintNumsCal.java
+++ JavaBasicProblems/src/loop/PrintNumsCal.java
@@ -7,5 +7,18 @@ public static void main(String[] args) {
 //			3*3*3=27
 //			4*4*4*4=256
 //			5*5*5*5*5=3125
+	int i,j;
+	//iterate nums 1-5
+	for(i=1;i<=5;i++) {
+		//print multiplication pattern
+		for(j=1;j<=i;j++) {
+			System.out.print(i);
+			if(j<i) {
+				//adding * bw nums
+				System.out.print("*");
+			}
+		}
+		System.out.println("="+(int)Math.pow(i, i));
+	}
 }
 }
