0d229d388e44b35c863905bd90c06f666d07c81a
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-25 22:45:18 +0530

    is sorted

--- JavaBasicProblems/src/Arrays/IsSorted.java
+++ JavaBasicProblems/src/Arrays/IsSorted.java
@@ -1,5 +1,16 @@
 package Arrays;
 
 public class IsSorted {
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 7, 9, 5};
+		boolean sorted = true;
+		for(int i=1; i<=arr.length; i++) {
+			if(arr[i] < arr[i-1]) {
+				sorted = false;
+				break;
+			}
+		}
+		System.out.println("Is sorted? - "+ sorted);
+	}
 
 }
