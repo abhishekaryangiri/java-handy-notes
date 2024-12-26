ec76962dc324457d35c4f8d0cf96ada055278f3e
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-26 15:36:37 +0530

    oops

---
+++ JavaBasicProblems/src/oops/oops01/Student.java
@@ -0,0 +1,22 @@
+package oops.oops01;
+
+public class Student {
+
+	//data: data member and also called instance var 
+	int studentdId;
+	String studentName;
+	String studentCity;
+	
+	//behav. : member methods or methods
+	
+	public void study() {
+		System.out.println(studentName);
+	}
+	
+	public void fullDetails() {
+		System.out.println(studentName);
+		System.out.println(studentdId);
+		System.out.println(studentCity);
+	}
+}
+
