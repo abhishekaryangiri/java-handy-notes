2989442b3bca0383fc1359cc79dbc62fae120aa7
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-27 22:42:18 +0530

    kk

---
+++ DSAetom/src/java01loop/Loop2Print1to10WhileLoop.java
@@ -0,0 +1,11 @@
+package java01loop;
+
+public class Loop2Print1to10WhileLoop {
+public static void main(String[] args) {
+	int runner=1;
+	while(runner<=10) {
+		System.out.print(runner + " ");
+		runner++;
+	}
+}
+}
