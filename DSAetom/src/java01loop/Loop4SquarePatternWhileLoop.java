2a58e9ce802a7e1c6ae41b302b8ff903ae856ebd
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-27 22:51:51 +0530

    sqr patt

---
+++ DSAetom/src/java01loop/Loop4SquarePatternWhileLoop.java
@@ -0,0 +1,13 @@
+package java01loop;
+
+public class Loop4SquarePatternWhileLoop {
+
+	public static void main(String[] args) {
+		
+		int l = 1;
+		while(l<=4) {
+			System.out.println("# # # #");
+			l++;
+		}
+	}
+}
