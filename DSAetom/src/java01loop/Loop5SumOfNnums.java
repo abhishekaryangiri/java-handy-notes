e2f6f974bc4a6ce702538d8d7f1f8457d0d2c36c
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-28 23:53:54 +0530

    sun till nth no.

--- DSAetom/src/java01loop/Loop5SumOfNnums.java
+++ DSAetom/src/java01loop/Loop5SumOfNnums.java
@@ -1,5 +1,16 @@
 package java01loop;
 
 public class Loop5SumOfNnums {
-
+public static void main(String[] args) {
+	
+	int n;
+	n=3;
+	int sum=0;
+	int i = 1;
+	while(i<=n) {
+		sum+=i;
+		i++;
+	}
+	System.out.println(sum);
+}
 }
