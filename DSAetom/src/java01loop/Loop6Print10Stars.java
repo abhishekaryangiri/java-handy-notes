1b20eca21f20cb0dc50dda0a4337cdb3226504d8
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-01 22:47:13 +0530

    loop

--- DSAetom/src/java01loop/Loop6Print10Stars.java
+++ DSAetom/src/java01loop/Loop6Print10Stars.java
@@ -4,9 +4,10 @@ final class Loop6Print10Stars {
 	public static void main(String[] args) {
 		int count = 1;
 		while(count <= 10) {
-			System.out.print("*");
+			System.out.print("* ");
+			count++;
+		
 		}
-		System.out.println();
 	}
 
 }
