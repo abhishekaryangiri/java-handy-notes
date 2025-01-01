590d9e87ed97473467f5d20288a3613d51f31c6e
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-01 22:44:15 +0530

    loop

---
+++ DSAetom/src/java01loop/Loop6Print10Stars.java
@@ -0,0 +1,12 @@
+package java01loop;
+
+final class Loop6Print10Stars {
+	public static void main(String[] args) {
+		int count = 1;
+		while(count <= 10) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+
+}
---
+++ Javascript/interview-problems/problem3.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/*console.log(typeof typeof 100)*/
+
+/*const arr = [...'abhi']
+console.log(arr)*/
+
+/*[ 'a', 'b', 'h', 'i' ]*/
+
+
+console.log(parseInt('10+2'))
