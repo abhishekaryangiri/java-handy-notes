aad302d73294f2523fb968d9bf1687ab4730f15d
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-01 22:54:44 +0530

    loop end

---
+++ DSAetom/src/java01loop/Loop6PrintSqare.java
@@ -0,0 +1,13 @@
+package java01loop;
+
+public class Loop6PrintSqare {
+	public static void main(String[] args) {
+		for( int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print( " * " );
+			}
+			System.out.println();
+		}
+	}
+
+}
