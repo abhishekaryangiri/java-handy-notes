168445642761e3659abcb9e1d978658bc347d5d7
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-04 00:00:06 +0530

    inverted patt

--- DSAetom/src/java01loop/Loop7InvertwdRightAngleTrianbgle.java
+++ DSAetom/src/java01loop/Loop7InvertwdRightAngleTrianbgle.java
@@ -1,5 +1,12 @@
 package java01loop;
 
 public class Loop7InvertwdRightAngleTrianbgle {
-
+public static void main(String[] args) {
+	for(int i=5;i>=1; i--) {
+		for(int j=1;j<=i;j++) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+}
 }
