b352056cc20395447b98662c7988d263f25865f6
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-04 16:30:05 +0530

    inverted triangle

--- DSAetom/src/java01loop/Loop7InvertwdRightAngleTrianbgle.java
+++ DSAetom/src/java01loop/Loop7InvertwdRightAngleTrianbgle.java
@@ -2,11 +2,11 @@ package java01loop;
 
 public class Loop7InvertwdRightAngleTrianbgle {
 public static void main(String[] args) {
-	for(int i=5;i>=1; i--) {
-		for(int j=1;j<=i;j++) {
+	for(int i=5; i>=1; i--) {
+		for(int j=1; j<=i; j++) {
 			System.out.print(" * ");
 		}
-		System.out.println();
+		/* System.out.println(); */
 	}
 }
 }
