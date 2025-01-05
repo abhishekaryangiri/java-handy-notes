8a165b18c4192f0477bf944704a3a3a55195af1b
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-05 22:46:20 +0530

    reverse a num

--- DSAetom/src/java01loop/Loop20ReaverseOfaNum.java
+++ DSAetom/src/java01loop/Loop20ReaverseOfaNum.java
@@ -1,5 +1,13 @@
 package java01loop;
 
 public class Loop20ReaverseOfaNum {
-
+public static void main(String[] args) {
+	int dob = 10101995;
+	while(dob>0) {
+		int rev = dob % 10;
+		System.out.print(rev);
+		dob = dob/10;
+	}
+	System.out.println();
+}
 }
