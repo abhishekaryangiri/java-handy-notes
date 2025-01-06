5ab9aaae8844766c45ff47824e88be0358acb32a
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-06 23:45:00 +0530

    jj

---
+++ DSAetom/src/java01loop/Loop12CheckPrime.java
@@ -0,0 +1,8 @@
+package java01loop;
+
+public class Loop12CheckPrime {
+public static void main(String[] args) {
+	boolean isPrime = true;
+	for(int i=2; i<=)
+}
+}
--- DSAetom/src/java01loop/Loop20ReaverseOfaNum.java
+++ DSAetom/src/java01loop/Loop20ReaverseOfaNum.java
@@ -2,11 +2,11 @@ package java01loop;
 
 public class Loop20ReaverseOfaNum {
 public static void main(String[] args) {
-	int dob = 10101995;
-	while(dob>0) {
-		int rev = dob % 10;
-		System.out.print(rev);
-		dob = dob/10;
+	int initialDigit = 10101995;
+	while(initialDigit>0) {
+		int reverse = initialDigit % 10;
+		System.out.print(reverse);
+		initialDigit = initialDigit/10;
 	}
 	System.out.println();
 }
