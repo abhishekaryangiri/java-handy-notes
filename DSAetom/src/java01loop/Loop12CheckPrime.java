ad7cdc15b3d48ee159812b0cafb92a2d4637241f
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-06 23:54:07 +0530

    isPrime

--- DSAetom/src/java01loop/Loop12CheckPrime.java
+++ DSAetom/src/java01loop/Loop12CheckPrime.java
@@ -2,7 +2,17 @@ package java01loop;
 
 public class Loop12CheckPrime {
 public static void main(String[] args) {
+	int num = 19;
 	boolean isPrime = true;
-	for(int i=2; i<=)
+	if(num<2) {
+		isPrime = false;
+	}
+	for(int i=2; i<=num; i++) {
+		if(num % i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	System.out.println("isPrime? " + ": " +isPrime );
 }
 }
