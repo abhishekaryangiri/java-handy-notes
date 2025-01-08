d3deea8a9cc04736a8ff10233261aa7032eb4021
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-08 22:51:02 +0530

    multi d

--- DSAetom/src/arrays/Arr02MultidiamArr.java
+++ DSAetom/src/arrays/Arr02MultidiamArr.java
@@ -1,5 +1,14 @@
 package arrays;
 
-public class Arr02MultidiamArr {
+import java.util.Arrays;
 
+public class Arr02MultidiamArr {
+	public static void main(String[] args) {
+		int[][] mArrays = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+		System.out.println(Arrays.deepToString(mArrays));
+	}
 }
