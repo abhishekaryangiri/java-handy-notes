6c93ee44c7e7084fd86df1891de78079713c82db
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-08 22:45:54 +0530

    arrays

---
+++ DSAetom/src/arrays/Arr01Array.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Arr01Array {
+	public static void main(String[] args) {
+		String name[] = {"Ram", "Shayam", "Radha"};
+		int nums[] = {1,2,3,4,5};
+		
+		System.out.println(Arrays.toString(name));
+		System.out.println(Arrays.toString(nums));
+		System.out.println(name[0]);
+		System.out.println(nums[2]);
+	}
+
+}
