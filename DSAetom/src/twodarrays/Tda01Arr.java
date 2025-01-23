88525b97fbd584dd3cbe1d273917de3036a8f4d3
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-23 23:40:44 +0530

    aa

---
+++ DSAetom/src/twodarrays/Tda01Arr.java
@@ -0,0 +1,23 @@
+package twodarrays;
+
+import java.util.Arrays;
+//iniialize a array
+public class Tda01Arr {
+public static void main(String[] args) {
+	//way:1
+	int[] arr = new int[3];
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	System.out.println(Arrays.toString(arr));
+	
+	//way:2
+	int[] arr2 = {1,2,3,4,5};
+	System.out.println(Arrays.toString(arr2));
+	
+	//way:3
+	
+}
+
+
+}
