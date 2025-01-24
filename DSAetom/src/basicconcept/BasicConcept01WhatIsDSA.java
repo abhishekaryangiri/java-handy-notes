85f6b81e3eb2a5d4c42e61bab8cdc0042ce671d5
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-24 13:54:27 +0530

    data str

---
+++ DSAetom/src/basicconcept/BasicConcept01WhatIsDSA.java
@@ -0,0 +1,21 @@
+package basicconcept;
+
+public class BasicConcept01WhatIsDSA {
+public static void main(String[] args) {
+	//1. Data Str: this is the way /methodology to store the data and we can do operations like searching , inttnn, del etc.
+	
+	//a.uses of data structure: quick access of data
+	//b. to break large problems into smaller
+	
+	//2. time and space complexity: algo is working how fast and how much memory is needed for it.
+	
+	//real ife ex.: 
+	//a. O(n) time complexity: sup. we have 100 file and we have to search 1 file-- if we will search  1-1  it will  takes lots of time. Its VCalled O(n).
+	//O(log n): if files are sorted and  using bimnry search , then it will take less time, which is called O(log n);
+	
+	//Space compexity: if we are using another memory to resolve a  problem. it cause SC.
+	//ex. - if we r creating a new list and copy data, it takes extra memory.
+	//ex if we want to search a person cont num in mobile. if list is sorted it will take less time, o9log n . 
+	//if it is unsorted takes more time O(n).
+}
+}
