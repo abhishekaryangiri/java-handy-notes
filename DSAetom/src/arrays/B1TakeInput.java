9bf75c071d7feeb90319eff8196e351e4c6285ce
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-03-15 23:31:15 +0530

    take input

--- DSAetom/src/arrays/B1TakeInput.java
+++ DSAetom/src/arrays/B1TakeInput.java
@@ -1,5 +1,24 @@
 package arrays;
 
-public class B1TakeInput {
+import java.util.Scanner;
 
+public class B1TakeInput {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int[] numbers = new int[5];
+	System.out.println("Enter 5 numbers");
+	for(int i =0;i<5;i++) {
+		numbers[i] = sc.nextInt();
+	}
+	System.out.println("the numbers are: ");
+	for(int  n=0;n<5;n++) {
+		System.out.println(numbers[n]+ " ");
+	}
+	sc.close();
 }
+}
+//intitution:
+//1: take scanner class
+//2: declair size of arr
+//3: loop till size
+//4: loop for output
